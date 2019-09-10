94e5de52d9a2f3f41be77fd0d4b34bb87f5f2c72
Andrew Christensen <andrewpchristensen701@gmail.com>
2019-09-10 10:09:57 -0400

    final commit

--- dotm_search.py
+++ dotm_search.py
@@ -5,12 +5,47 @@
 Given a directory path, search all files in the path for a given text string
 within the 'word/document.xml' section of a MSWord .dotm file.
 """
-__author__ = "???"
+__author__ = "andrewpchristensen701"
 
+from zipfile import ZipFile 
+import argparse
+import os
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='searches dotm files for text')
+    parser.add_argument('--dir', help = 'directory to search', default='.') 
+    parser.add_argument('text', help='text to search for')
+    return parser
+
+# directory
 
 def main():
-    raise NotImplementedError("Your awesome code begins here!")
+    parser = create_parser()
+    args = parser.parse_args()
+    print args
 
+    fileList = os.listdir(args.dir)
 
+    print fileList
+    total_counter = 0
+    match_counter = 0
+
+    for file_0 in fileList: 
+        if file_0.endswith('dotm'):
+            with ZipFile(args.dir + '/' + file_0) as zip:
+
+                with zip.open('word/document.xml') as doc:
+                    total_counter += 1
+                    lines = doc.read()
+                    if lines.find(args.text) != -1:
+                        print ("Match found in file: " +args.dir + '/'  + file_0 + '\n' 
+                            + lines[lines.find(args.text)-40:lines.find(args.text)+40] + "\n")
+                        match_counter += 1
+                    
+    print ("Total dotm files searched: " +  match_counter)
+    print ("Total dotm files matched: " + total_counter)
+
+                                
 if __name__ == '__main__':
     main()
